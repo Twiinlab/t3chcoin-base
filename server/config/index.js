642eac91bfb44a8cd934185b5ffa19e1de9dacb3
ruben chavarri <ruben.chavarri.contractor@bbva.com>
2018-02-26 22:42:41 +0100

    update instance address

--- server/config/index.js
+++ server/config/index.js
@@ -12,7 +12,7 @@ var config = {
             provider: 'http://104.199.25.52:8545/' //'http://35.205.133.70:8545/'
         }
     },
-    smartContractInstance: '0xffdca7ead87c9289e2f943254ac165015a859d46', //'0xf68a76f9176302c9db228e41064505a2c42cf274' //default value
+    smartContractInstance: '0xf68a76f9176302c9db228e41064505a2c42cf274', //'0xffdca7ead87c9289e2f943254ac165015a859d46', //'0xf68a76f9176302c9db228e41064505a2c42cf274' //default value
 };
 
 module.exports.blockchain = config[env].blockchain;
